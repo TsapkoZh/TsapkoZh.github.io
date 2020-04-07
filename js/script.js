bc4cbc686dd2bcb500cf986bfb1e81a9e0c9bed5
TsapkoZh <TsapkoZh@gmail.com>
2020-04-07 18:15:10 +0300

    jsFix

--- js/script.js
+++ js/script.js
@@ -1,56 +1,64 @@
-
-
-
-// block-anim-swipe
+// blockAnimSwipe
 // ===============================
 window.onload = function() {
     document.querySelector('.js-main__heroes-bike').classList.add('block_swipe');
     document.querySelector('.js-main__gallery').classList.add('block_swipe');
 };
 
-var lastScrollTop = 0;
-window.addEventListener(
-    "scroll",
-    function() {
-        let st = window.pageYOffset || document.documentElement.scrollTop; 
-        if (st > 600) {
-            document.querySelector('.js-about__about-text-btn').classList.add('block_swipe');
-            document.querySelector('.js-about__about-img-scale').classList.add('block_swipe');
-        } 
-        if (st > 1200) {
-            document.querySelector('.js-summer__summer-img-scale').classList.add('block_swipe');
-            document.querySelector('.js-summer__summer-title-btn').classList.add('block_swipe');
-        } 
-    }
-);
-
-
-
-// hide-mobil-menu-scroll
+// mobilMenuScroll
 // ===============================
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.querySelector('.js-nav-bar-mobil__btn-wrapper').classList.remove('nav-bar-mobil__btn-wrapper_hide');
-        } else {
-            document.querySelector('.js-nav-bar-mobil__btn-wrapper').classList.add('nav-bar-mobil__btn-wrapper_hide');
-            document.querySelector('.js-nav-bar-mobil__menu-lines').classList.remove('nav-bar-mobil__menu-line_swipe');
-            document.querySelector('.js-nav-bar-mobil__burger-icon').classList.remove('nav-bar-mobil__burger-icon_active');
+window.addEventListener('scroll', throttle(handleScroll, 100), false);
+
+function throttle(fn, wait) {
+    let time = Date.now();
+    return function() {
+      if ((time + wait - Date.now()) < 0) {
+        fn();
+        time = Date.now();
+      }
     }
-    prevScrollpos = currentScrollPos;
+  }
+
+let prevScrollPos = window.pageYOffset;
+
+function handleScrollAnimation(currentScrollPos) {
+     if (currentScrollPos > 600) {
+        document.querySelector('.js-about__about-text-btn').classList.add('block_swipe');
+        document.querySelector('.js-about__about-img-scale').classList.add('block_swipe');
+    }
+    if (currentScrollPos > 1200) {
+        document.querySelector('.js-summer__summer-img-scale').classList.add('block_swipe');
+        document.querySelector('.js-summer__summer-title-btn').classList.add('block_swipe');
+    }
 }
 
+function handleHiddenMenu(currentScrollPos) {
+    if (prevScrollPos > currentScrollPos) {
+        document.querySelector('.js-nav-bar-mobil__btn-wrapper').classList.remove('nav-bar-mobil__btn-wrapper_hide');
+    } else {
+        document.querySelector('.js-nav-bar-mobil__btn-wrapper').classList.add('nav-bar-mobil__btn-wrapper_hide');
+        document.querySelector('.js-nav-bar-mobil__menu-lines').classList.remove('nav-bar-mobil__menu-line_swipe');
+        document.querySelector('.js-nav-bar-mobil__burger-icon').classList.remove('nav-bar-mobil__burger-icon_active');
+    }
+}
 
+function handleScroll() {
+    const currentScrollPos = window.pageYOffset;
 
-// mobil-menu-btn
+    handleHiddenMenu(currentScrollPos);
+    handleScrollAnimation(currentScrollPos);
+
+    prevScrollPos = currentScrollPos;
+};
+
+// mobilMenuBtn
 // ===============================
-document.querySelector('.js-nav-bar-mobil__btn').addEventListener('click', () => {
+document.querySelector('.js-nav-bar-mobil__btn').addEventListener('click', function() {
     document.querySelector('.js-nav-bar-mobil__menu-lines').classList.toggle('nav-bar-mobil__menu-line_swipe');
     document.querySelector('.js-nav-bar-mobil__burger-icon').classList.toggle('nav-bar-mobil__burger-icon_active');
 });
 
-document.querySelector('.js-nav-bar-mobil__menu-line_4').addEventListener('click', () => {
+document.querySelector('.js-nav-bar-mobil__menu-line_4').addEventListener('click', function() {
     document.querySelector('.js-nav-bar-mobil__menu-lines').classList.remove('nav-bar-mobil__menu-line_swipe');
     document.querySelector('.js-nav-bar-mobil__burger-icon').classList.remove('nav-bar-mobil__burger-icon_active');
-});
+})
